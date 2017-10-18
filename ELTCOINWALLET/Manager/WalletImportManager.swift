58a25f4a8c2f900415d7ff2884b489b03ae52f77
olliem36 <olivermahoney@me.com>
2017-10-18 09:33:05 +0100

    started wallet import js apis

---
+++ ELTCOINWALLET/Manager/WalletImportManager.swift
@@ -0,0 +1,22 @@
+//
+//  WalletImportManager.swift
+//  ELTCOINWALLET
+//
+//  Created by Oliver Mahoney on 18/10/2017.
+//  Copyright © 2017 ELTCOIN. All rights reserved.
+//
+
+import Foundation
+
+class WalletImportManager {
+    static let sharedInstance = WalletImportManager()
+    private init() {}
+    
+    let importWalletBrowserURL = "/dist/index.html#view-wallet-info"
+}
+
+extension WalletImportManager {
+    
+}
+
+
